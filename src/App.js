8fba2e20fa15b974fceeefe3c1a2852b1a3abfe6
BradyHeFF <heffernanbrady@gmail.com>
2023-09-21 22:21:16 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect} from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipesList from './RecipesList';
 import SubmitRecipeForm from './SubmitRecipeForm';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
+    const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => {
-    fetch("/recipes.json")
-      .then(response => response.json())
-      .then(data => setRecipes(data))
-      .catch(error => console.error("Error fetching the recipes:", error));
-  }, []);
-  
+    useEffect(() => {
+        fetch("/recipes.json")
+            .then(response => response.json())
+            .then(data => setRecipes(data))
+            .catch(error => console.error("Error fetching the recipes:", error));
+    }, []);
+
     const addRecipe = (recipe) => {
         setRecipes(prevRecipes => [...prevRecipes, { ...recipe, id: prevRecipes.length + 1 }]);
     };
@@ -25,16 +28,26 @@ function App() {
     return (
         <Router>
             <div className="App">
-                <nav>
-                    <Link to="/">All Recipes</Link>
-                    <Link to="/submit">Submit Recipe</Link>
-                </nav>
-
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <LinkContainer to="/">
+                                <Nav.Link>All Recipes</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/submit">
+                                <Nav.Link>Submit Recipe</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <div className="padding">
                 <Routes>
                     <Route path="/" element={<RecipesList recipes={recipes} onRemove={removeRecipe} />} />
                     <Route path="/submit" element={<SubmitRecipeForm onAddRecipe={addRecipe} />} />
                 </Routes>
             </div>
+        </div>
         </Router>
     );
 }
--- src/SubmitRecipeForm.js
+++ src/SubmitRecipeForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 function SubmitRecipeForm({ onAddRecipe }) {
     const [formData, setFormData] = useState({
@@ -32,35 +33,43 @@ function SubmitRecipeForm({ onAddRecipe }) {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-            </label>
-            <label>
-                Ingredients (comma separated):
-                <input type="text" name="ingredients" value={formData.ingredients} onChange={handleChange} required />
-            </label>
-            <label>
-                Directions:
-                <textarea name="directions" value={formData.directions} onChange={handleChange} required></textarea>
-            </label>
-            <label>
-                Description:
-                <textarea name="description" value={formData.description} onChange={handleChange} required></textarea>
-            </label>
-            <label>
-                Image:
-                <select name="image" value={formData.image} onChange={handleChange}>
-                    <option value="/images/cinnamon-buns.jpg">Cinnamon Buns</option>
-                    <option value="/images/coffee-cake.jpg">Coffee Cake</option>
-                    <option value="/images/chocolate-danish.jpg">Chocolate Danish</option>
-                    <option value="/images/chocolate-chip-cookies.jpg">Chocolate Chip Cookies</option>
-                </select>
-            </label>
-            <button type="submit">Submit Recipe</button>
-        </form>
+        <div className="container mt-5">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control type="text" name="name" value={formData.name} onChange={handleChange} required />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Ingredients (comma separated):</Form.Label>
+                    <Form.Control type="text" name="ingredients" value={formData.ingredients} onChange={handleChange} required />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Directions:</Form.Label>
+                    <Form.Control as="textarea" name="directions" value={formData.directions} onChange={handleChange} required />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Description:</Form.Label>
+                    <Form.Control as="textarea" name="description" value={formData.description} onChange={handleChange} required />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Image:</Form.Label>
+                    <Form.Select name="image" value={formData.image} onChange={handleChange}>
+                        <option value="/images/cinnamon-buns.jpg">Cinnamon Buns</option>
+                        <option value="/images/coffee-cake.jpg">Coffee Cake</option>
+                        <option value="/images/chocolate-danish.jpg">Chocolate Danish</option>
+                        <option value="/images/chocolate-chip-cookies.jpg">Chocolate Chip Cookies</option>
+                    </Form.Select>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">Submit Recipe</Button>
+            </Form>
+        </div>
     );
 }
 
 export default SubmitRecipeForm;
+
